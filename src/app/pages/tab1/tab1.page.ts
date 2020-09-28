5af868c56303560a26194655b02c990350035afb
Fabian Chinchilla <fb.argenis@gmail.com>
2020-09-27 19:19:47 -0600

    Mostrar registros en el historial

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { DataLocalService } from './../../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -9,20 +10,26 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 export class Tab1Page {
   opts = {
     allowSlidePrev: false,
-    allowSlideNext: false  
+    allowSlideNext: false
   }
-  constructor(private barcodeScanner: BarcodeScanner) { }
+  constructor(private barcodeScanner: BarcodeScanner, private dataService: DataLocalService) {
+  }
 
   scan() {
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+
+      if (!barcodeData.cancelled) {
+        this.dataService.saveRegistry(barcodeData.format, barcodeData.text);
+      }
     }).catch(err => {
-      console.log('Error', err);
+      console.log('Error', err);      
+      this.dataService.saveRegistry("QRCode", "https://www.qrcode.es/es/generador-qr-code/");
     });
   }
 
   ionViewWillEnter() {
-
+    this.scan();
   }
   ionViewDidEnter() {
 
